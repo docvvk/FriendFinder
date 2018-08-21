56aaf707f9b233fea9a93591da7a847874ef1a7a
Vivek Malhan <vivek.malhan@yahoo.com>
2018-08-21 14:49:04 -0400

    routes commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+//Friend Finder Survey (DATA)
+//=================================================================
+var friends = [{
+    name: "Mark Sanchez",
+    photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fb.fssta.com%2Fuploads%2Fapplication%2Fnfl%2Fplayers%2F297127.vresize.350.425.medium.22.png&imgrefurl=https%3A%2F%2Fwww.foxsports.com%2Fnfl%2Fmark-sanchez-player-stats&docid=qFDDHJNZTda0wM&tbnid=wLCsl4bFGK3zTM%3A&vet=10ahUKEwjN6paDtK7bAhUBJKwKHcAAAccQMwg5KAQwBA..i&w=350&h=425&bih=637&biw=1366&q=mark%20sanchez&ved=0ahUKEwjN6paDtK7bAhUBJKwKHcAAAccQMwg5KAQwBA&iact=mrc&uact=8",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+},
+{
+    name: "Beyonce Knowles",
+    photo: "https://nyppagesix.files.wordpress.com/2018/05/beyonce-solange.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+    scores: [
+        "1",
+        "1",
+        "4",
+        "4",
+        "5",
+        "5",
+        "3",
+        "3",
+        "2",
+        "1"
+    ]
+},
+{
+    name: "Hillary Clinton",
+    photo: "http://thepoliticalcarnival.net/2012/12/15/hillary-clinton-faints-has-concussion/hillary-rodham-clinton/",
+    scores: [
+        "1",
+        "1",
+        "4",
+        "4",
+        "5",
+        "5",
+        "3",
+        "3",
+        "2",
+        "1"
+    ]
+},
+]
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+//Stores friends database into friends variable
+var friends = require("../data/friends");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", (req,res) => {
+        res.join(friends);
+    })
+
+    app.post("/api/friends", (req,res) => {
+
+        var matchLogic = {};
+
+        var matchScoreToBeat = 100;
+
+        for (var i = 0; i < friends.length; i++) {
+            
+            var mathArray = [];
+            var totalDifference = 0;
+
+            for (var x = 0; x < friends[i].scores.length; x++) {
+
+                mathArray.push(Math.abs(req.body.scores[x] - friends[i].scores[x]));
+            };
+
+            for (var y = 0; y < mathArray.length; y++) {
+                totalDifference += mathArray[y];
+            }
+
+            if (matchLogic == {}) {
+                matchLogic = friends[i];
+                matchScoreToBeat = totalDifference;
+            } else if (totalDifference < matchScoreToBeat) {
+                match = friends[i];
+                matchScoreToBeat = totalDifference;
+            }
+        }
+
+        console.log('Your match is: ' + match.name);
+
+        // Push new person into friends array
+        friends.push(req.body);
+        res.json(match);
+    });
+}
